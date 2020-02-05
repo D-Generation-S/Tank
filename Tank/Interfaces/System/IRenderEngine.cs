27685be45ba89ec7a97d557fa8f452d54f386be8
XanatosX <simonaberle@gmx.de>
2020-02-05 22:26:35 +0100

    Adding new method to render engine

--- Tank/Interfaces/System/IRenderEngine.cs
+++ Tank/Interfaces/System/IRenderEngine.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Tank.Interfaces.Components;
+using Tank.Interfaces.Entity;
 using Tank.Interfaces.Implementations;
 
 namespace Tank.Interfaces.System
 {
     interface IRenderEngine : IDrawable
     {
+        void AddRenderer(IEntity entity);
+
         void AddRenderer(IVisible visibleEntity);
 
         void AddRenderer(IRenderer renderer);
