ed378a59bd38a5ed13f4764529e39351296129e5
XanatosX <simonaberle@gmx.de>
2020-02-05 21:23:34 +0100

    Adding new interfaces
    Adding interfaces for entites, position, velocity and the physic engine

---
+++ Tank/Interfaces/Entity/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank.Interfaces.Entity
+{
+    interface IEntity : IUpdateable
+    {
+        Vector2 Position { get; }
+
+        void Reset();
+    }
+}
---
+++ Tank/Interfaces/System/IPhysicEngine.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Interfaces.Entity;
+
+namespace Tank.Interfaces.System
+{
+    interface IPhysicEngine : IUpdateable
+    {
+        void AddPhysicObject(IPhysicEntity physicEntity);
+    }
+}
