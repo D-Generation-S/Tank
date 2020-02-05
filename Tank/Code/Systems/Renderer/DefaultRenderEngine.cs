cee819c65b658cc88faaa3446fc0a5a616e33ccd
XanatosX <simonaberle@gmx.de>
2020-02-05 21:44:14 +0100

    Adding default render engine to the project

---
+++ Tank/Code/Systems/Renderer/DefaultRenderEngine.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Interfaces.Components;
+using Tank.Interfaces.Implementations;
+using Tank.Interfaces.System;
+
+namespace Tank.Code.Systems.Renderer
+{
+    class DefaultRenderEngine : IRenderEngine
+    {
+        private readonly List<IRenderer> renderObjects;
+        private readonly SpriteBatch spriteBatch;
+
+        private int drawOrder;
+        public int DrawOrder => drawOrder;
+
+        private bool visible;
+        public bool Visible => visible;
+
+        public event EventHandler<EventArgs> DrawOrderChanged;
+        public event EventHandler<EventArgs> VisibleChanged;
+
+        public DefaultRenderEngine(SpriteBatch spriteBatch)
+        {
+            this.spriteBatch = spriteBatch;
+            renderObjects = new List<IRenderer>();
+        }
+
+        public void AddRenderer(IVisible visibleEntity)
+        {
+            AddRenderer(visibleEntity.Renderer);
+        }
+
+        public void AddRenderer(IRenderer renderer)
+        {
+            renderObjects.Add(renderer);
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin(SpriteSortMode.BackToFront);
+            spriteBatch.End();
+        }
+    }
+}
