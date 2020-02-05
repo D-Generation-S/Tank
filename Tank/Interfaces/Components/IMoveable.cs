4e1083c49442dee56c966b011d810e4181284ff9
XanatosX <simonaberle@gmx.de>
2020-02-05 21:23:34 +0100

    Adding new interfaces
    Adding interfaces for entites, position, velocity and the physic engine

---
+++ Tank/Interfaces/Components/IMoveable.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank.Interfaces.Components
+{
+    interface IMoveable
+    {
+        Vector2 Velocity { get; set; }
+    }
+}
