d58143624c63112be16d25bbb66ff54c121afeff
XanatosX <simonaberle@gmx.de>
2020-02-06 22:15:34 +0100

    Adding two different renderer

---
+++ Tank/Code/Implementations/SpriteSheetRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.DataContainer;
+
+namespace Tank.Code.Implementations
+{
+    class SpriteSheetRenderer : SpriteRenderer
+    {
+        private readonly Position singleImageSize;
+        private readonly int additionalDistance;
+        protected readonly Position position;
+
+        public SpriteSheetRenderer(Position singleImageSize, int additionalDistance, Position position)
+        {
+            this.singleImageSize = singleImageSize;
+            this.additionalDistance = additionalDistance;
+            this.position = position;
+        }
+
+        protected override void BuildSourceRectangle()
+        {
+            int positionX = singleImageSize.X * position.X + additionalDistance * position.X;
+            int positionY = singleImageSize.Y * position.Y + additionalDistance * position.Y;
+
+            source = new Rectangle(positionX, positionY, singleImageSize.X, singleImageSize.Y);
+        }
+    }
+}
