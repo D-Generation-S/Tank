67f643b43f9d0acc2f328f614c0f8dd7ea504e49
XanatosX <simonaberle@gmx.de>
2020-02-06 22:15:58 +0100

    Adding new projectile class and builder

---
+++ Tank/Code/Entities/Weapon/Projectile.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.BaseClasses;
+using Tank.Interfaces.Components;
+using Tank.Interfaces.Entity;
+using Tank.Interfaces.Implementations;
+
+namespace Tank.Code.Entities.Weapon
+{
+    class Projectile : BaseEntity, IVisibleEntity, IPhysicEntity
+    {
+        private IRenderer renderer;
+        public IRenderer Renderer => renderer;
+
+        private Vector2 position;
+        public Vector2 Position {
+            get => position;
+            set => position = value; 
+        }
+
+        private Vector2 velocity;
+        public Vector2 Velocity
+        {
+            get => velocity;
+            set => velocity = value;
+        }
+        
+        public Projectile(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public override void Initzialize(string uniqueName)
+        {
+            active = true;
+            alive = true;
+            base.Initzialize(uniqueName);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            renderer.Position = position;
+        }
+    }
+}
