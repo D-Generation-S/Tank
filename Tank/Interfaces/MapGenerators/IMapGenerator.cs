0e50103ca8c02d5e14d78ad87378ccaecc349ae7
XanatosX <simonaberle@gmx.de>
2020-02-07 20:01:21 +0100

    Starting to add interfaces for maps
    Starting to switchj map to entity construct

---
+++ Tank/Interfaces/MapGenerators/IMapGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.DataContainer;
+
+namespace Tank.Interfaces.MapGenerators
+{
+    interface IMapGenerator
+    {
+        IMap GenerateNewMap(Position size, int seed);
+
+        IMap AsyncGenerateNewMap(Position size, int seed);
+    }
+}
