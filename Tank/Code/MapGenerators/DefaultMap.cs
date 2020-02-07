c7e8c26871b17981d04d5e2b8eda6d7a8948d9a2
XanatosX <simonaberle@gmx.de>
2020-02-07 20:01:21 +0100

    Starting to add interfaces for maps
    Starting to switchj map to entity construct

---
+++ Tank/Code/MapGenerators/DefaultMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.BaseClasses;
+using Tank.Code.DataContainer;
+using Tank.Interfaces.MapGenerators;
+
+namespace Tank.Code.MapGenerators
+{
+    class DefaultMap : BaseEntity, IMap
+    {
+        private Texture2D image;
+        public Texture2D Image => image;
+
+        private FlattenArray<bool> collissionMap;
+        public FlattenArray<bool> CollisionMap => collissionMap;
+
+        public DefaultMap(Texture2D image, FlattenArray<bool> collistionMap)
+        {
+            
+        }
+    }
+}
---
+++ Tank/Interfaces/MapGenerators/IMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.DataContainer;
+using Tank.Interfaces.Entity;
+
+namespace Tank.Interfaces.MapGenerators
+{
+    interface IMap : IEntity
+    {
+        /// <summary>
+        /// The image to draw the map on the screen
+        /// </summary>
+        Texture2D Image { get; }
+
+        /// <summary>
+        /// The collision data for this map
+        /// </summary>
+        FlattenArray<bool> CollisionMap { get; }
+    }
+}
