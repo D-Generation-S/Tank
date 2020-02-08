eaab1b16e80f85750a1626a560e699239e7b9a4e
XanatosX <simonaberle@gmx.de>
2020-02-08 14:23:16 +0100

    Adding content to the interface

--- Tank/Interfaces/MapGenerators/IMapTexturizer.cs
+++ Tank/Interfaces/MapGenerators/IMapTexturizer.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tank.Interfaces.Random;
 
 namespace Tank.Interfaces.MapGenerators
 {
     interface IMapTexturizer
     {
+        /// <summary>
+        /// This will texturize a given map
+        /// </summary>
+        /// <param name="map">The map to texturize</param>
+        void TexturizeMap(IMap map);
+
+        /// <summary>
+        /// This will texturize a given map
+        /// </summary>
+        /// <param name="map">The map to texturize</param>
+        /// <param name="randomizer">The randomizer to use</param>
+        void TexturizeMap(IMap map, IRandom randomizer);
     }
 }
