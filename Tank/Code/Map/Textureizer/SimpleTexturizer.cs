0db0da64e9fd367260845a419b40240eac4578c0
XanatosX <simonaberle@gmx.de>
2020-02-08 14:44:17 +0100

    Renaming file
    Adding simple logic to texturize the output
    Extending the interface

---
+++ Tank/Code/Map/Textureizer/SimpleTexturizer.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank.Code.DataContainer;
+using Tank.Interfaces.MapGenerators;
+using Tank.Interfaces.Random;
+
+namespace Tank.Code.Textureizer
+{
+    class DefaultTextureizer : IMapTexturizer
+    {
+        private readonly SpriteSheet spriteSheet;
+        private Random internalRandomizer;
+
+        private IRandom randomizer;
+
+        private int spriteYPosition;
+        private int spriteXPosition;
+
+        public DefaultTextureizer(SpriteSheet spriteSheet)
+        {
+            this.spriteSheet = spriteSheet;
+        }
+
+        public void TexturizeMap(IMap map, Color generatorFillColor)
+        {
+            TexturizeMap(map, generatorFillColor, null);
+        }
+
+        public void TexturizeMap(IMap map, Color generatorFillColor, IRandom randomizer)
+        {
+            spriteYPosition = 0;
+            spriteXPosition = 0;
+
+            this.randomizer = randomizer;
+            internalRandomizer = randomizer == null ? new Random(map.Seed) : null;
+
+            Color[] colorsToUse = spriteSheet.GetColorFromSprite(0, 0);
+            FlattenArray<Color> colors = new FlattenArray<Color>(colorsToUse, spriteSheet.SingleImageSize.X);
+
+            for (int x = 0; x < map.Width; x++)
+            {
+                if (spriteXPosition > spriteSheet.SingleImageSize.X - 1)
+                {
+                    spriteXPosition = 0;
+                }
+                for (int y = 0; y < map.Height; y++)
+                {
+                    if (spriteYPosition > spriteSheet.SingleImageSize.Y - 1)
+                    {
+                        spriteYPosition = 0;
+                    }
+
+                    if (map.GetPixel(x, y) == generatorFillColor)
+                    {
+                        map.ChangePixel(x, y, colors.GetValue(spriteXPosition, spriteYPosition));
+                    }
+                    spriteYPosition++;
+                }
+                spriteXPosition++;
+            }
+
+            map.ApplyChanges();
+
+            return;
+        }
+
+        private float GetRandomNumber(float minimum, float maximum)
+        {
+            if (randomizer != null)
+            {
+                return randomizer.GetNewNumber(minimum, maximum);
+            }
+
+            return (float)internalRandomizer.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
--- Tank/Interfaces/MapGenerators/IMapTexturizer.cs
+++ Tank/Interfaces/MapGenerators/IMapTexturizer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,13 +14,15 @@ namespace Tank.Interfaces.MapGenerators
         /// This will texturize a given map
         /// </summary>
         /// <param name="map">The map to texturize</param>
-        void TexturizeMap(IMap map);
+        /// <param name="generatorFillColor">The color the generator did use for creating the map</param>
+        void TexturizeMap(IMap map, Color generatorFillColor);
 
         /// <summary>
         /// This will texturize a given map
         /// </summary>
         /// <param name="map">The map to texturize</param>
+        /// <param name="generatorFillColor">The color the generator did use for creating the map</param>
         /// <param name="randomizer">The randomizer to use</param>
-        void TexturizeMap(IMap map, IRandom randomizer);
+        void TexturizeMap(IMap map, Color generatorFillColor, IRandom randomizer);
     }
 }
