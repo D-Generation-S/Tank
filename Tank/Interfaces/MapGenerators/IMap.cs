a60ccdb603720de8f16717e9abeecb9c33659c54
XanatosX <simonaberle@gmx.de>
2020-02-08 08:46:27 +0100

    Extending the interfaces
    Adding summary blocks

--- Tank/Interfaces/MapGenerators/IMap.cs
+++ Tank/Interfaces/MapGenerators/IMap.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,51 @@ namespace Tank.Interfaces.MapGenerators
         /// <summary>
         /// The collision data for this map
         /// </summary>
-        FlattenArray<bool> CollisionMap { get; }
+        FlattenArray<bool> CollissionMap { get; }
+
+        /// <summary>
+        /// Add or replace a new collideable pixel
+        /// </summary>
+        /// <param name="position">The position of the new pixel</param>
+        /// <param name="color">Thje color of the new pixel</param>
+        void AddPixel(Position position, Color color);
+
+        /// <summary>
+        /// Add or replace a new pixel on a given location
+        /// </summary>
+        /// <param name="position">The position to add the pixel</param>
+        /// <param name="color">The color of the pixel to add</param>
+        /// <param name="collidable">Is this pixel collideable</param>
+        void AddPixel(Position position, Color color, bool collidable);
+
+        /// <summary>
+        /// Add or replace a new collideable pixel
+        /// </summary>
+        /// <param name="x">The x position of the pixel</param>
+        /// <param name="y">The y position of the pixel</param>
+        /// <param name="color">The color of the pixel</param>
+        void AddPixel(int x, int y, Color color);
+
+        /// <summary>
+        /// Add or replace a new pixel on a given location
+        /// </summary>
+        /// <param name="x">The x position of the pixel</param>
+        /// <param name="y">The y position of the pixel</param>
+        /// <param name="color">The color of the pixel to add</param>
+        /// <param name="collidable">Is this pixel collideable</param>
+        void AddPixel(int x, int y, Color color, bool collidable);
+
+        /// <summary>
+        /// Remove a pixel at a given position
+        /// </summary>
+        /// <param name="position">The position to remove the pixel from</param>
+        void RemovePixel(Position position);
+
+        /// <summary>
+        /// Remove a pixel at a given position
+        /// </summary>
+        /// <param name="x">The x position of the pixel</param>
+        /// <param name="y">The y position of the pixel</param>
+        void RemovePixel(int x, int y);
     }
 }
--- Tank/Interfaces/MapGenerators/IMapGenerator.cs
+++ Tank/Interfaces/MapGenerators/IMapGenerator.cs
@@ -9,8 +9,20 @@ namespace Tank.Interfaces.MapGenerators
 {
     interface IMapGenerator
     {
+        /// <summary>
+        /// This will generate you a new map to use
+        /// </summary>
+        /// <param name="size">The size of the map</param>
+        /// <param name="seed">The seed of the map</param>
+        /// <returns></returns>
         IMap GenerateNewMap(Position size, int seed);
 
+        /// <summary>
+        /// This will async generate you a new map to use
+        /// </summary>
+        /// <param name="size">The size of the map</param>
+        /// <param name="seed">The seed of the map</param>
+        /// <returns></returns>
         IMap AsyncGenerateNewMap(Position size, int seed);
     }
 }
