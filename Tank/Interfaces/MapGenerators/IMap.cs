2ffd96f6a6e94c658b66bbd0eb6a37e26d3f1541
XanatosX <simonaberle@gmx.de>
2020-02-08 13:45:00 +0100

    Extending the interfaces

--- Tank/Interfaces/MapGenerators/IMap.cs
+++ Tank/Interfaces/MapGenerators/IMap.cs
@@ -18,6 +18,16 @@ namespace Tank.Interfaces.MapGenerators
         int Seed { get; }
 
         /// <summary>
+        /// The height of the map
+        /// </summary>
+        int Height { get; }
+
+        /// <summary>
+        /// The width of the map
+        /// </summary>
+        int Width { get; }
+
+        /// <summary>
         /// The image to draw the map on the screen
         /// </summary>
         Texture2D Image { get; }
@@ -32,7 +42,7 @@ namespace Tank.Interfaces.MapGenerators
         /// </summary>
         /// <param name="position">The position of the new pixel</param>
         /// <param name="color">Thje color of the new pixel</param>
-        void AddPixel(Position position, Color color);
+        void SetPixel(Position position, Color color);
 
         /// <summary>
         /// Add or replace a new pixel on a given location
@@ -40,7 +50,7 @@ namespace Tank.Interfaces.MapGenerators
         /// <param name="position">The position to add the pixel</param>
         /// <param name="color">The color of the pixel to add</param>
         /// <param name="collidable">Is this pixel collideable</param>
-        void AddPixel(Position position, Color color, bool collidable);
+        void SetPixel(Position position, Color color, bool collidable);
 
         /// <summary>
         /// Add or replace a new collideable pixel
@@ -48,7 +58,8 @@ namespace Tank.Interfaces.MapGenerators
         /// <param name="x">The x position of the pixel</param>
         /// <param name="y">The y position of the pixel</param>
         /// <param name="color">The color of the pixel</param>
-        void AddPixel(int x, int y, Color color);
+        void SetPixel(int x, int y, Color color);
+
 
         /// <summary>
         /// Add or replace a new pixel on a given location
@@ -57,7 +68,63 @@ namespace Tank.Interfaces.MapGenerators
         /// <param name="y">The y position of the pixel</param>
         /// <param name="color">The color of the pixel to add</param>
         /// <param name="collidable">Is this pixel collideable</param>
-        void AddPixel(int x, int y, Color color, bool collidable);
+        void SetPixel(int x, int y, Color color, bool collidable);
+
+        /// <summary>
+        /// This will change a pixel in cache for later applying
+        /// </summary>
+        /// <param name="position">The position of the pixel to change</param>
+        /// <param name="color">The new color of the pixel</param>
+        void ChangePixel(Position position, Color color);
+
+        /// <summary>
+        /// This will change a pixel in cache for later applying
+        /// </summary>
+        /// <param name="position">The position of the pixel to change</param>
+        /// <param name="color">The new color of the pixel</param>
+        /// <param name="collidable">Is this new pixel collideable</param>
+        void ChangePixel(Position position, Color color, bool collidable);
+
+        /// <summary>
+        /// This will change a pixel in cache for later applying
+        /// </summary>
+        /// <param name="x">The x position of the pixel to change</param>
+        /// <param name="y">The y position of the pixel to change</param>
+        /// <param name="color">The new color of the pixel</param>
+        void ChangePixel(int x, int y, Color color);
+
+        /// <summary>
+        /// This will change a pixel in cache for later applying
+        /// </summary>
+        /// <param name="x">The x position of the pixel to change</param>
+        /// <param name="y">The y position of the pixel to change</param>
+        /// <param name="color">The new color of the pixel</param>
+        /// <param name="collidable">Is this new pixel collideable</param>
+        void ChangePixel(int x, int y, Color color, bool collidable);
+
+        /// <summary>
+        /// This will revert all the changes done by the change pixel method
+        /// </summary>
+        void RevertChanges();
+
+        /// <summary>
+        /// This will apply all the changes done by the change pixel method
+        /// </summary>
+        void ApplyChanges();
+
+        /// <summary>
+        /// Get the pixel at a given position
+        /// </summary>
+        /// <param name="position">The position to get the pixel from</param>
+        Color GetPixel(Position position);
+
+        /// <summary>
+        /// Get the pixel at a given postion
+        /// </summary>
+        /// <param name="x">The x position of the pixel to get</param>
+        /// <param name="y">The y position of the pixel to get</param>
+        /// <returns></returns>
+        Color GetPixel(int x, int y);
 
         /// <summary>
         /// Remove a pixel at a given position
