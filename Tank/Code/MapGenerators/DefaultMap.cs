9ae91a1722b892e288ad11020b803512ea0b6628
XanatosX <simonaberle@gmx.de>
2020-02-08 13:45:10 +0100

    Extending the default map to match new interface

--- Tank/Code/MapGenerators/DefaultMap.cs
+++ Tank/Code/MapGenerators/DefaultMap.cs
@@ -18,11 +18,18 @@ namespace Tank.Code.MapGenerators
 
         private FlattenArray<Color> imageData;
 
+        private FlattenArray<Color> changedImageData;
+        private FlattenArray<bool> changedCollisionMap;
+
         private FlattenArray<bool> collissionMap;
         public FlattenArray<bool> CollissionMap => collissionMap;
 
         private readonly int seed;
         public int Seed => seed;
+
+        public int Height => image.Height;
+
+        public int Width => image.Width;
 
         public DefaultMap(Texture2D image, FlattenArray<bool> collissionMap)
             : this(image, collissionMap, 0)
@@ -42,27 +49,84 @@ namespace Tank.Code.MapGenerators
             this.seed = seed;
         }
 
-        public void AddPixel(Position position, Color color)
+        public void SetPixel(Position position, Color color)
         {
-            AddPixel(position.X, position.Y, color, true);
+            SetPixel(position.X, position.Y, color, true);
         }
 
-        public void AddPixel(Position position, Color color, bool collidable)
+        public void SetPixel(Position position, Color color, bool collidable)
         {
-            AddPixel(position.X, position.Y, color, collidable);
+            SetPixel(position.X, position.Y, color, collidable);
         }
 
-        public void AddPixel(int x, int y, Color color)
+        public void SetPixel(int x, int y, Color color)
         {
-            AddPixel(x, y, color, true);
+            SetPixel(x, y, color, true);
         }
 
-        public void AddPixel(int x, int y, Color color, bool collidable)
+        public void SetPixel(int x, int y, Color color, bool collidable)
         {
             imageData.SetValue(x, y, color);
             collissionMap.SetValue(x, y, collidable);
 
             image.SetData(imageData.Array);
+        }
+
+        public void ChangePixel(Position position, Color color)
+        {
+            ChangePixel(position.X, position.Y, color, true);
+        }
+
+        public void ChangePixel(Position position, Color color, bool collidable)
+        {
+            ChangePixel(position.X, position.Y, color, collidable);
+        }
+
+        public void ChangePixel(int x, int y, Color color)
+        {
+            ChangePixel(x, y, color, true);
+        }
+
+        public void ChangePixel(int x, int y, Color color, bool collidable)
+        {
+            if (changedCollisionMap == null)
+            {
+                changedCollisionMap = new FlattenArray<bool>(collissionMap.Array, Width);
+            }
+                
+            if (changedImageData == null)
+            {
+                changedImageData = new FlattenArray<Color>(imageData.Array, Width);
+            }
+
+            changedImageData.SetValue(x, y, color);
+            changedCollisionMap.SetValue(x, y, collidable);
+        }
+
+        public void RevertChanges()
+        {
+            changedImageData = null;
+            changedCollisionMap = null;
+        }
+
+        public void ApplyChanges()
+        {
+            imageData = changedImageData;
+            image.SetData<Color>(imageData.Array);
+
+            collissionMap = changedCollisionMap;
+
+            RevertChanges();
+        }
+
+        public Color GetPixel(Position position)
+        {
+            return GetPixel(position.X, position.Y);
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            return imageData.GetValue(x, y);
         }
 
         public void RemovePixel(Position position)
