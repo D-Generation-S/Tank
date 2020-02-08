ecf4e0907620fb49af59e5b3da3e2c5e32b2d3e9
XanatosX <simonaberle@gmx.de>
2020-02-08 09:39:42 +0100

    Adding new interfaces

---
+++ Tank/Interfaces/Random/IRandom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank.Interfaces.Random
+{
+    interface IRandom
+    {
+        /// <summary>
+        /// Initzialize the randomizer
+        /// </summary>
+        /// <param name="seed"></param>
+        void Initzialize(int seed);
+
+        /// <summary>
+        /// Get a new random number
+        /// </summary>
+        /// <returns></returns>
+        float GetNewNumber();
+
+        /// <summary>
+        /// Get a new random number in between
+        /// </summary>
+        /// <param name="minValue">Lowest possible value for the number</param>
+        /// <param name="maxValue">Hightest possible value for the number</param>
+        /// <returns></returns>
+        float GetNewNumber(float minValue, float maxValue);
+    }
+}
