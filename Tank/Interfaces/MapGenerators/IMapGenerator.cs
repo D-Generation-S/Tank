5c4f0d819d44009ed0f52135bfb244df2e915fee
XanatosX <simonaberle@gmx.de>
2020-02-08 13:45:00 +0100

    Extending the interfaces

--- Tank/Interfaces/MapGenerators/IMapGenerator.cs
+++ Tank/Interfaces/MapGenerators/IMapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,14 @@ namespace Tank.Interfaces.MapGenerators
 {
     interface IMapGenerator
     {
+        Color MapColor { get; }
+
+        /// <summary>
+        /// This method will allow you to set a map color as you want
+        /// </summary>
+        /// <param name="newMapColor"></param>
+        void SetMapColor(Color newMapColor);
+
         /// <summary>
         /// This will generate you a new map to use
         /// </summary>
@@ -28,17 +37,33 @@ namespace Tank.Interfaces.MapGenerators
         /// This will generate you a new map to use
         /// </summary>
         /// <param name="size">The size of the map</param>
-        /// <param name="seed">The seed to use</param>
         /// <param name="mapTexturizer">The instance to use to texturize the map</param>
+        /// <param name="seed">The seed to use</param>
         /// <returns></returns>
-        IMap GenerateNewMap(Position size, int seed, IMapTexturizer mapTexturizer);
+        IMap GenerateNewMap(Position size, IMapTexturizer mapTexturizer, int seed);
 
         /// <summary>
         /// This will async generate you a new map to use
         /// </summary>
         /// <param name="size">The size of the map</param>
+        /// <returns></returns>
+        Task<IMap> AsyncGenerateNewMap(Position size);
+
+        /// <summary>
+        /// This will async generate you a new map to use
+        /// </summary>
+        /// <param name="size">The size of the map</param>
+        /// <param name="mapTexturizer">The texturizer to use</param>
+        /// <returns></returns>
+        Task<IMap> AsyncGenerateNewMap(Position size, IMapTexturizer mapTexturizer);
+
+        /// <summary>
+        /// This will async generate you a new map to use
+        /// </summary>
+        /// <param name="size">The size of the map</param>
+        /// <param name="mapTexturizer">The texturizer to use</param>
         /// <param name="seed">The seed of the map</param>
         /// <returns></returns>
-        Task<IMap> AsyncGenerateNewMap(Position size, int seed, IMapTexturizer mapTexturizer);
+        Task<IMap> AsyncGenerateNewMap(Position size, IMapTexturizer mapTexturizer, int seed);
     }
 }
