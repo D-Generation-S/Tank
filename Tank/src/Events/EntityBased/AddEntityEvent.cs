0c34652d6bf79e7039459da7cdb0dd6acc8b5928
XanatosX <simonaberle@gmx.de>
2020-02-15 08:53:00 +0100

    Adding new event to add new entites

---
+++ Tank/src/Events/EntityBased/AddEntityEvent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank.src.Events.EntityBased
+{
+    class AddEntityEvent : EntityBasedEvent
+    {
+        private readonly List<IGameComponent> components;
+        public List<IGameComponent> Components => components;
+
+        public AddEntityEvent(uint entityId) : base(entityId)
+        {
+        }
+
+
+        public AddEntityEvent(uint entityId, List<IGameComponent> components) : base(entityId)
+        {
+            this.components = components;
+        }
+    }
+}
