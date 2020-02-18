edc9336e1ae691044cc0a42afb111be5c9a0bf0d
XanatosX <simonaberle@gmx.de>
2020-02-18 18:39:16 +0100

    Adding missing summary blocks

--- Tank/src/Interfaces/EntityComponentSystem/Manager/IGameEngine.cs
+++ Tank/src/Interfaces/EntityComponentSystem/Manager/IGameEngine.cs
@@ -8,13 +8,25 @@ using Tank.src.Wrapper;
 
 namespace Tank.src.Interfaces.EntityComponentSystem.Manager
 {
+    /// <summary>
+    /// This interface represents the game engine itself
+    /// </summary>
     interface IGameEngine
     {
+        /// <summary>
+        /// The event manager to use in the game engine
+        /// </summary>
         IEventManager EventManager { get; }
 
+        /// <summary>
+        /// The entity manager to use in the game engine
+        /// </summary>
         IEntityManager EntityManager { get; }
 
-        ContentWrapper ContentManager { get;  }
+        /// <summary>
+        /// In instance of the content wrapper to load or get content from cache or loading it
+        /// </summary>
+        ContentWrapper ContentManager { get; }
 
         /// <summary>
         /// This method allows you to add a new system
