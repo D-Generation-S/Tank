8669749701d1d6ccf7ebcd1b730ca4850299ce0d
XanatosX <simonaberle@gmx.de>
2020-02-18 19:28:50 +0100

    Adding class summary

--- Tank/src/Events/TerrainEvents/DamageTerrainEvent.cs
+++ Tank/src/Events/TerrainEvents/DamageTerrainEvent.cs
@@ -7,11 +7,25 @@ using Tank.src.DataStructure;
 
 namespace Tank.src.Events.TerrainEvents
 {
+    /// <summary>
+    /// This class is an event telling the system there should be some terrain damage
+    /// </summary>
     class DamageTerrainEvent : EventArgs
     {
+        /// <summary>
+        /// The area where the damage should be applied to
+        /// </summary>
         private readonly Circle damageArea;
+
+        /// <summary>
+        /// Readonly access to the area where the damage should be applied to
+        /// </summary>
         public Circle DamageArea => damageArea;
 
+        /// <summary>
+        /// Create a new instance of this event
+        /// </summary>
+        /// <param name="damageArea"></param>
         public DamageTerrainEvent(Circle damageArea)
         {
             this.damageArea = damageArea;
