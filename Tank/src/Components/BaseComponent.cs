c21151097f16f456f677ea2336a8ae821bd2b412
XanatosX <simonaberle@gmx.de>
2020-02-18 19:48:41 +0100

    Adding summary blocks

--- Tank/src/Components/BaseComponent.cs
+++ Tank/src/Components/BaseComponent.cs
@@ -7,14 +7,32 @@ using Tank.src.Interfaces.EntityComponentSystem;
 
 namespace Tank.src.Components
 {
+    /// <summary>
+    /// This class represents a component base class making the implementation easier
+    /// </summary>
     class BaseComponent : IComponent
     {
+        /// <summary>
+        /// The id of the entity the component is assigned to
+        /// </summary>
         private uint entityId;
+
+        /// <summary>
+        /// Readonly access to the entity id the component is assigned to
+        /// </summary>
         public uint EntityId => entityId;
 
+        /// <summary>
+        /// Allow multiple instances of this component per entity
+        /// </summary>
         protected bool allowMultiple;
+
+        /// <summary>
+        /// Readonly access if multiple component instances are allowed per entity
+        /// </summary>
         public bool AllowMultiple => allowMultiple;
 
+        /// <inheritdoc/>
         public void SetEntityId(uint newId)
         {
             entityId = newId;
