4c5037d7103d59dba2ae9ccca975833896c640ea
XanatosX <simonaberle@gmx.de>
2020-02-18 19:57:32 +0100

    Adding summary to classes

--- Tank/src/Components/MapComponent.cs
+++ Tank/src/Components/MapComponent.cs
@@ -7,11 +7,25 @@ using Tank.Interfaces.MapGenerators;
 
 namespace Tank.src.Components
 {
+    /// <summary>
+    /// This class will make an entity to an map component
+    /// </summary>
     class MapComponent : BaseComponent
     {
+        /// <summary>
+        /// An instance of a map to use for the game
+        /// </summary>
         private IMap map;
+
+        /// <summary>
+        /// Public readonly access to the map instance
+        /// </summary>
         public IMap Map => map;
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="map">The map instance to use</param>
         public MapComponent(IMap map)
         {
             this.map = map;
