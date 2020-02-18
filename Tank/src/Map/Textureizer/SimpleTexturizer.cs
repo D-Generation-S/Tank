5df34bb6650c963002d0dd158c6516f4de038574
XanatosX <simonaberle@gmx.de>
2020-02-18 21:12:27 +0100

    Adding summary blocks

--- Tank/src/Map/Textureizer/SimpleTexturizer.cs
+++ Tank/src/Map/Textureizer/SimpleTexturizer.cs
@@ -11,26 +11,61 @@ using Tank.src.Interfaces.Randomizer;
 
 namespace Tank.src.Code.Textureizer
 {
+    /// <summary>
+    /// This class will texturize the map with one texture only
+    /// </summary>
     class DefaultTextureizer : IMapTexturizer
     {
+        /// <summary>
+        /// The spritesheet to use
+        /// </summary>
         private readonly SpriteSheet spriteSheet;
+
+        /// <summary>
+        /// The internal randomizer if nothing was set
+        /// </summary>
         private Random internalRandomizer;
 
+        /// <summary>
+        /// The randomizer to use
+        /// </summary>
         private IRandomizer randomizer;
 
+        /// <summary>
+        /// The y position of the sprite to use
+        /// </summary>
         private int spriteYPosition;
+
+        /// <summary>
+        /// The x position of the sprite to use
+        /// </summary>
         private int spriteXPosition;
 
+        /// <summary>
+        /// Create a new instance of the class
+        /// </summary>
+        /// <param name="spriteSheet">The spritesheet to use</param>
         public DefaultTextureizer(SpriteSheet spriteSheet)
         {
             this.spriteSheet = spriteSheet;
         }
 
+        /// <summary>
+        /// Texturize the map
+        /// </summary>
+        /// <param name="map">The map to texturize</param>
+        /// <param name="generatorFillColor">The color the generator did use for drawing the map template</param>
         public void TexturizeMap(IMap map, Color generatorFillColor)
         {
             TexturizeMap(map, generatorFillColor, null);
         }
 
+        /// <summary>
+        /// Texturize the map
+        /// </summary>
+        /// <param name="map">The map to texturize</param>
+        /// <param name="generatorFillColor">The color the generator did use for drawing the map template</param>
+        /// <param name="randomizer">The randomizer to use</param>
         public void TexturizeMap(IMap map, Color generatorFillColor, IRandomizer randomizer)
         {
             spriteYPosition = 0;
@@ -70,6 +105,12 @@ namespace Tank.src.Code.Textureizer
             return;
         }
 
+        /// <summary>
+        /// Get a random number
+        /// </summary>
+        /// <param name="minimum">The minimum number to generate</param>
+        /// <param name="maximum">The maximum number to generate</param>
+        /// <returns>A random float number</returns>
         private float GetRandomNumber(float minimum, float maximum)
         {
             if (randomizer != null)
