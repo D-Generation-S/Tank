6efcc51a36d5c0518702e887d484cabff9b00eb4
XanatosX <simonaberle@gmx.de>
2020-02-18 19:10:07 +0100

    Correcting class bugs
    Adding summary

--- Tank/src/Wrapper/ContentWrapper.cs
+++ Tank/src/Wrapper/ContentWrapper.cs
@@ -7,14 +7,28 @@ using System.Threading.Tasks;
 
 namespace Tank.src.Wrapper
 {
+    /// <summary>
+    /// This class will warp the content manager
+    /// </summary>
     class ContentWrapper
     {
+        /// <summary>
+        /// The Monogame content manager to use
+        /// </summary>
         private readonly ContentManager contentManager;
-        public ContentManager Content;
 
+        /// <summary>
+        /// Read access to the Monogame content manager
+        /// </summary>
+        public ContentManager Content => contentManager;
+
+        /// <summary>
+        /// Create a new instance of the wrapper
+        /// </summary>
+        /// <param name="contentManager"></param>
         public ContentWrapper(ContentManager contentManager)
         {
-            contentManager = contentManager;
+            this.contentManager = contentManager;
         }
     }
 }
