a4352d383e2f10011ddc89fd2e0edee494d308c7
XanatosX <simonaberle@gmx.de>
2020-02-18 19:57:32 +0100

    Adding summary to classes

--- Tank/src/Components/MoveableComponent.cs
+++ Tank/src/Components/MoveableComponent.cs
@@ -7,22 +7,42 @@ using System.Threading.Tasks;
 
 namespace Tank.src.Components
 {
+    /// <summary>
+    /// Make a entity moveable
+    /// </summary>
     class MoveableComponent : BaseComponent
     {
+        /// <summary>
+        /// The velocity of the entity
+        /// </summary>
         private Vector2 velocity;
+
+        /// <summary>
+        /// Public access to the velocity
+        /// </summary>
         public Vector2 Velocity
         {
             get => velocity;
             set => velocity = value;
         }
 
+        /// <summary>
+        /// Is the entity on the ground
+        /// </summary>
         private bool onGround;
+
+        /// <summary>
+        /// Public access if the entity is on the ground
+        /// </summary>
         public bool OnGround
         {
             get => onGround;
             set => onGround = value;
         }
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
         public MoveableComponent()
         {
             allowMultiple = false;
