2df6968e1fe7edae293fa10635a509251c4f011f
XanatosX <simonaberle@gmx.de>
2020-02-18 19:34:51 +0100

    Adding Summary blocks

--- Tank/src/Events/ComponentBased/NewComponentEvent.cs
+++ Tank/src/Events/ComponentBased/NewComponentEvent.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Tank.src.Events.ComponentBased
 {
+    /// <summary>
+    /// This event will tell the systems that a new component was added
+    /// </summary>
     class NewComponentEvent : EntityBasedEvent
     {
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="entityId">The id of the entity where a component was added to</param>
         public NewComponentEvent(uint entityId) : base(entityId)
         {
         }
