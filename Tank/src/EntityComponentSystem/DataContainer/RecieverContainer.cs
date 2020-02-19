80efc5d1188454d96ecc4c8d010a6355eef3f6fa
XanatosX <simonaberle@gmx.de>
2020-02-19 19:36:57 +0100

    Adding missing Summary blocks

--- Tank/src/EntityComponentSystem/DataContainer/RecieverContainer.cs
+++ Tank/src/EntityComponentSystem/DataContainer/RecieverContainer.cs
@@ -7,12 +7,29 @@ using Tank.src.Interfaces.EntityComponentSystem.Manager;
 
 namespace Tank.src.EntityComponentSystem.DataContainer
 {
+    /// <summary>
+    /// A container for a set of recievers assigned to an type
+    /// </summary>
     class RecieverContainer
     {
+        /// <summary>
+        /// Readonly type of the notification this container is based on
+        /// </summary>
         private readonly Type notificationType;
+
+        /// <summary>
+        /// Public readonly access to the notification type of this container
+        /// </summary>
         public Type NotificationType => notificationType;
 
+        /// <summary>
+        /// A readonly list of all the receivers assigned to this event type
+        /// </summary>
         private readonly List<IEventReceiver> receivers;
+
+        /// <summary>
+        /// A readonly access to all the assigned receivers
+        /// </summary>
         public List<IEventReceiver> EventReceivers => receivers;
 
         /// <summary>
