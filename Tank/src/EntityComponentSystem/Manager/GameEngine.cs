be437a25191bd522b8ce884430eda89f82160af5
XanatosX <simonaberle@gmx.de>
2020-02-19 19:43:42 +0100

    Adding summary blocks

--- Tank/src/EntityComponentSystem/Manager/GameEngine.cs
+++ Tank/src/EntityComponentSystem/Manager/GameEngine.cs
@@ -10,19 +10,52 @@ using Tank.src.Wrapper;
 
 namespace Tank.src.EntityComponentSystem.Manager
 {
+    /// <summary>
+    /// This class is a container for all the game engine components
+    /// </summary>
     class GameEngine : IGameEngine
     {
+        /// <summary>
+        /// A instance of the event manager to use in the current game engine
+        /// </summary>
         private readonly IEventManager eventManager;
+
+        /// <summary>
+        /// Readonly access to the event manager for classes from the outside
+        /// </summary>
         public IEventManager EventManager => eventManager;
 
+        /// <summary>
+        /// A instance of the entity manager to use in the current game engine
+        /// </summary>
         private readonly IEntityManager entityManager;
+
+        /// <summary>
+        /// Readonly access to the entity manager for external reading
+        /// </summary>
         public IEntityManager EntityManager => entityManager;
 
+        /// <summary>
+        /// A warpper class to provide the game engine some content
+        /// </summary>
         private readonly ContentWrapper contentManager;
+
+        /// <summary>
+        /// Readonly access to the content manager for loading new content into the game
+        /// </summary>
         public ContentWrapper ContentManager => contentManager;
 
+        /// <summary>
+        /// A private list of all the systems currently registered
+        /// </summary>
         private readonly List<ISystem> systems;
 
+        /// <summary>
+        /// Create a new instance of the game engine
+        /// </summary>
+        /// <param name="eventManager">The event manager to use</param>
+        /// <param name="entityManager">The entity manager to use</param>
+        /// <param name="contentWrapper">The content warpper to use</param>
         public GameEngine(IEventManager eventManager, IEntityManager entityManager, ContentWrapper contentWrapper)
         {
             systems = new List<ISystem>();
@@ -32,12 +65,14 @@ namespace Tank.src.EntityComponentSystem.Manager
             contentManager = contentWrapper;
         }
 
+        /// <inheritdoc/>
         public void AddSystem(ISystem systemToAdd)
         {
             systemToAdd.Initialize(this);
             systems.Add(systemToAdd);
         }
 
+        /// <inheritdoc/>
         public void Update(GameTime gameTime)
         {
             foreach (ISystem system in systems)
@@ -46,6 +81,7 @@ namespace Tank.src.EntityComponentSystem.Manager
             }
         }
 
+        /// <inheritdoc/>
         public void Draw(GameTime gameTime)
         {
             foreach (ISystem system in systems)
