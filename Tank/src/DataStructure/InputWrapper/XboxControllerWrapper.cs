7bfcc3b0a476f7ee081649d4c5d8eb7740fba9b5
XanatosX <simonaberle@gmx.de>
2020-03-13 11:28:44 +0100

    Starting to add controller support

---
+++ Tank/src/DataStructure/InputWrapper/XboxControllerWrapper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tank.src.DataStructure.InputWrapper
+{
+    class XboxControllerWrapper
+    {
+        private readonly PlayerIndex index;
+
+        private GamePadState currentPadState;
+
+        public XboxControllerWrapper(PlayerIndex index)
+        {
+            this.index = new PlayerIndex();
+        }
+
+        public void UpdateState()
+        {
+            //previousKeyState = currentKeyState;
+            currentPadState = GamePad.GetState(index);
+        }
+
+        public bool IsKeyDown()
+        {
+            return currentPadState.Triggers.Right > 0;
+        }
+    }
+}
---
+++ Tank/src/DataStructure/StaticXboxController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Tank.src.DataStructure.InputWrapper;
+using Tank.src.Interfaces.GameObjectControlling;
+
+namespace Tank.src.DataStructure
+{
+    class StaticXboxController : IGameObjectController
+    {
+        private readonly XboxControllerWrapper xboxControllerWrapper;
+
+        public StaticXboxController()
+        {
+            xboxControllerWrapper = new XboxControllerWrapper(Microsoft.Xna.Framework.PlayerIndex.One);
+        }
+
+        public bool DecreseStrength()
+        {
+            return xboxControllerWrapper.IsKeyDown();
+        }
+
+        public bool IncreaseStrength()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFirePressed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RotateDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RotateUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStates()
+        {
+            xboxControllerWrapper.UpdateState();
+        }
+    }
+}
