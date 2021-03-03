52263207be4113b8b57017084690e94aca3bab26
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-03-03 18:29:39 +0100

    Adding summary block

--- src/Tank/DataManagement/Loader/IDataLoader.cs
+++ src/Tank/DataManagement/Loader/IDataLoader.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Tank.Wrapper;
+﻿using Tank.Wrapper;
 
 namespace Tank.DataManagement.Loader
 {
+    /// <summary>
+    /// A simple data loader interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     interface IDataLoader<T>
     {
+        /// <summary>
+        /// Initzialize the data loader with the content wrapper
+        /// </summary>
+        /// <param name="contentWrapper">The content wrapper to use</param>
         void Init(ContentWrapper contentWrapper);
+
+        /// <summary>
+        /// Load the requested data
+        /// </summary>
+        /// <param name="fileName">The filename to get the data from</param>
+        /// <returns>The requested data</returns>
         T LoadData(string fileName);
     }
 }
