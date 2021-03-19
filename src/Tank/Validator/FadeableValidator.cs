28d2d900454e3a123616a5b037a2fd6777db7ea9
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-03-19 20:25:08 +0100

    Add summar block

--- src/Tank/Validator/FadeableValidator.cs
+++ src/Tank/Validator/FadeableValidator.cs
@@ -3,8 +3,12 @@ using Tank.Interfaces.EntityComponentSystem.Manager;
 
 namespace Tank.Validator
 {
+    /// <summary>
+    /// This class will validate any entity which will fade out or fade in
+    /// </summary>
     class FadeableValidator : RenderableEntityValidator
     {
+        /// <inheritdoc/>
         public override bool IsValidEntity(uint entityId, IEntityManager entityManager)
         {
             return base.IsValidEntity(entityId, entityManager) && entityManager.HasComponent<FadeComponent>(entityId);
