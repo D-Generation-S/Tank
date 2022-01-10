f6a60fe97fa45ca37dc9ec20f168b6a72b2d844c
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-01-10 08:50:09 +0100

    Trying to solve the problem with damage not getting dealt

--- src/Tank/Systems/DamageSystem.cs
+++ src/Tank/Systems/DamageSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using Tank.Components;
 using Tank.Components.GameObject;
 using Tank.Components.Tags;
@@ -110,26 +111,39 @@ namespace Tank.Systems
             PlayerStatisticComponent playerStatisticComponent = null;
             if (projectileBindComponent != null)
             {
-                foreach (uint entityId in entityManager.GetEntitiesWithComponent<PlayerStatisticComponent>())
-                {
-                    BindComponent playerBindComponent = entityManager.GetComponent<BindComponent>(entityId);
-                    if (playerBindComponent.BoundEntityId == projectileBindComponent.BoundEntityId)
-                    {
-                        playerStatisticComponent = entityManager.GetComponent<PlayerStatisticComponent>(entityId);
-                        break;
-                    }
-                }
+                entityManager.GetEntitiesWithComponent<PlayerStatisticComponent>()
+                             .Select(id => entityManager.GetComponent<BindComponent>(id))
+                             .Where(bind => bind != null)
+                             .Where(bindComponent => bindComponent.BoundEntityId == projectileBindComponent.BoundEntityId)
+                             .Select(bindComponent => entityManager.GetComponent<PlayerStatisticComponent>(bindComponent.EntityId))
+                             .FirstOrDefault();
             }
 
             foreach (uint gameObject in objects)
             {
                 PlaceableComponent placeableComponent = entityManager.GetComponent<PlaceableComponent>(gameObject);
+                ColliderComponent collider = entityManager.GetComponent<ColliderComponent>(gameObject);
+                Rectangle collisionArea = collider == null ? Rectangle.Empty : collider.Collider;
                 GameObjectData gameObjectData = entityManager.GetComponent<GameObjectData>(gameObject);
 
-                if (placeableComponent == null || gameObjectData == null || !damageComponent.DamageArea.IsInInCircle(placeableComponent.Position))
+                if (placeableComponent == null || collider == null || gameObjectData == null)
                 {
                     continue;
                 }
+
+                collisionArea.Location += placeableComponent.Position.ToPoint();
+                List<Point> points = new List<Point>();
+                points.Add(new Point(collisionArea.Center.X, collisionArea.Top));
+                points.Add(new Point(collisionArea.Center.X, collisionArea.Bottom));
+                points.Add(new Point(collisionArea.Left, collisionArea.Y));
+                points.Add(new Point(collisionArea.Right, collisionArea.Y));
+                points.Add(collisionArea.Center);
+                bool doesCollide = points.Any(dot => damageComponent.DamageArea.IsInInCircle(dot));
+                if (!doesCollide)
+                {
+                    continue;
+                }
+
 
                 float armor = gameObjectData.Properties.ContainsKey("Armor") ? gameObjectData.Properties["Armor"] : 0;
                 float damageToTake = damageComponent.CenterDamageValue;
