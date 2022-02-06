1a0676a6618aea3dd07fa45bef0b1ce333856874
XanatosX <10531466+XanatosX@users.noreply.github.com>
2022-02-06 08:30:32 +0100

    Add missing summary blocks Cleanup classes

--- src/DebugFramework/Streaming/Clients/Udp/Communication/BaseUdpCommunicationClient.cs
+++ src/DebugFramework/Streaming/Clients/Udp/Communication/BaseUdpCommunicationClient.cs
@@ -4,31 +4,52 @@ using System.Net.Sockets;
 
 namespace DebugFramework.Streaming.Clients.Udp.Communication
 {
+    /// <summary>
+    /// Class to define a communcation udp client and a endpoint to use
+    /// </summary>
     public class BaseUdpCommunicationClient : BaseNetworkClient, IDisposable
     {
+        /// <summary>
+        /// The communcation client to use
+        /// </summary>
         protected readonly UdpClient communicationClient;
+
+        /// <summary>
+        /// The endpoint used for sending data to
+        /// </summary>
         protected readonly IPEndPoint usedEndpoint;
 
-        public BaseUdpCommunicationClient() : this(new UdpClient())
-        {
-        }
+        /// <summary>
+        /// Create a new instance of this class with a random udp client
+        /// </summary>
+        public BaseUdpCommunicationClient() : this(new UdpClient()) { }
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="listenIp">The ip address to listen to</param>
+        /// <param name="listenPort">The port to listen on</param>
         public BaseUdpCommunicationClient(IPAddress listenIp, int listenPort)
-            : this(new UdpClient(listenPort, AddressFamily.InterNetwork), new IPEndPoint(listenIp, listenPort))
-        {
+            : this(new UdpClient(listenPort, AddressFamily.InterNetwork), new IPEndPoint(listenIp, listenPort)) { }
 
-        }
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="clientToUse">The cline to use for listening</param>
+        public BaseUdpCommunicationClient(UdpClient clientToUse) : this(clientToUse, null) { }
 
-        public BaseUdpCommunicationClient(UdpClient clientToUse) : this(clientToUse, null)
-        {
-        }
-
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="clientToUse">The cline to use for listening</param>
+        /// <param name="endpoint">The endpoint to send data to or recieve from</param>
         protected BaseUdpCommunicationClient(UdpClient clientToUse, IPEndPoint endpoint)
         {
             communicationClient = clientToUse;
             usedEndpoint = endpoint;
         }
 
+        /// <inheritdoc/>
         public virtual void Dispose()
         {
             communicationClient.Dispose();
--- src/DebugFramework/Streaming/Clients/Udp/Communication/CommunicationPackage.cs
+++ src/DebugFramework/Streaming/Clients/Udp/Communication/CommunicationPackage.cs
@@ -1,38 +1,32 @@
-﻿using DebugFramework.DataTypes;
-using DebugFramework.Streaming.Package;
-using System;
+﻿using DebugFramework.Streaming.Package;
 using System.Net;
 
 namespace DebugFramework.Streaming.Clients.Udp.Communication
 {
+    /// <summary>
+    /// Class which combined the recieved data and the sender from which the data was recieved from
+    /// </summary>
     public class CommunicationPackage
     {
+        /// <summary>
+        /// The sender who send the data
+        /// </summary>
         public IPEndPoint Sender { get; }
+
+        /// <summary>
+        /// The data package which was send
+        /// </summary>
         public IDataPackage dataPackage { get; }
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="sender">Who did send the data package</param>
+        /// <param name="udpPackage">The package which was sendet</param>
         public CommunicationPackage(IPEndPoint sender, IDataPackage udpPackage)
         {
             Sender = sender;
             dataPackage = udpPackage;
-        }
-
-        public Type GetPackageType()
-        {
-            if (dataPackage == null)
-            {
-                return null;
-            }
-            BaseDataType baseData = dataPackage.GetPayload();
-            return baseData == null ? null : baseData.GetRealType();
-        }
-
-        public T GetPackageContent<T>() where T : BaseDataType
-        {
-            if (dataPackage == null)
-            {
-                return default;
-            }
-            return dataPackage.GetPayload<T>();
         }
     }
 }
